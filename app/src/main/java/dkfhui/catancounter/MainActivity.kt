e96a94ae18529178a75f5a40784a4d9205a1dabc
dkfhui <dkfhui@gmail.com>
2018-12-19 15:58:03 -0600

    Adding to hashMap works

--- app/src/main/java/dkfhui/catancounter/MainActivity.kt
+++ app/src/main/java/dkfhui/catancounter/MainActivity.kt
@@ -8,15 +8,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var input = 0;
+    val numberSet = hashMapOf<Int, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        init()
+        numberSetText.text = numberSet.toString()
+
         inputButton.setOnClickListener{
-            input = rollInput.text.toString().toInt()
-            Toast.makeText(this, input.toString(), Toast.LENGTH_LONG).show()
+            addData(rollInput.text.toString().toInt())
         }
+    }
+
+    fun init(){
+        for(i in 2..12) {
+            numberSet[i] = 0
+        }
+    }
+
+    fun addData(x: Int) {
+        when(x) {
+            in 2..12 -> numberSet[x] = numberSet[x]!!+1
+            else -> Toast.makeText(this, "Please input a valid number from 2 to 12", Toast.LENGTH_LONG)
+        }
+        numberSetText.text = numberSet.toString()
     }
 }
